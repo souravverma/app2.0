c263db39c5da9a6c695aa17a469018ea880cbf76
sourav <souravverma@gmail.com>
2022-06-01 12:57:07 +0530

    ok

---
+++ app2.0/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using app2._0.Models;
+using Microsoft.Extensions.Configuration;
+
+namespace app2._0.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IConfiguration configuration;
+        public HomeController(IConfiguration iConfig)
+        {
+
+            configuration = iConfig;
+        }
+        public IActionResult Index()
+        {
+            string dbConn2 = configuration.GetValue<string>("MySettings:DbConnection");
+            ViewBag.a = dbConn2;
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ app2.0/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace app2._0.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
